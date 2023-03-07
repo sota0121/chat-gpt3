db42191dc55685bf4bccf308ba395d21f9b1eb32
sota_masuda <sota.m777121@gmail.com>
2023-03-07 23:41:39 +0900

    load openai api key from .env

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Load .env file
+	fileName := ".env"
+	err := godotenv.Load(fileName)
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		os.Exit(1)
+	}
+
+	openaiApiKey := os.Getenv("OPENAI_API_KEY")
+	fmt.Println("OpenAI API Key: ", openaiApiKey)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sota0121/chat-gpt3
 
 go 1.20
+
+require github.com/joho/godotenv v1.5.1 // indirect
